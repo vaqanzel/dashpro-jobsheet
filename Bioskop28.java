7d6637ad087798198d98c27dd7f0082fdf9ca27c
vaqanzel <valengrehasta@gmail.com>
2023-11-01 21:20:40 +0700

    Modifikasi 1

--- Bioskop28.java
+++ Bioskop28.java
@@ -16,10 +16,9 @@ public class Bioskop28 {
     System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
     System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
-    System.out.println(penonton.length);  
-    System.out.println(penonton[0].length);    
-    System.out.println(penonton[1].length);  
-    System.out.println(penonton[2].length); 
-    System.out.println(penonton[3].length); 
+    System.out.println(penonton.length);
+      for (int i = 0; i < penonton.length; i++) {
+    System.out.println("Panjang baris ke-" + (i+1) + ": "+ penonton[i].length);
+     }
 }
 }
