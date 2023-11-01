53f814b3b567bad24cea1115463ef8bff1a2a533
vaqanzel <valengrehasta@gmail.com>
2023-11-01 21:18:53 +0700

    Melengkapi kode program

--- Bioskop28.java
+++ Bioskop28.java
@@ -9,10 +9,17 @@ public class Bioskop28 {
     penonton [2][0] = "Eka";
     penonton [2][1] = "Farhan";
     penonton [3][0] = "Gisel";
+    penonton [3][1] = "Hana";
 
-    System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);  
-    System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);    
-    System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);    
-    System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);      
-    }
+    System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
+    System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
+    System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
+    System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+
+    System.out.println(penonton.length);  
+    System.out.println(penonton[0].length);    
+    System.out.println(penonton[1].length);  
+    System.out.println(penonton[2].length); 
+    System.out.println(penonton[3].length); 
+}
 }
