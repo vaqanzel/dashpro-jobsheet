a977b32755bb10d45062c9efbad09ef03863785c
vaqanzel <valengrehasta@gmail.com>
2023-11-02 20:22:56 +0700

    modifikasi 1

--- BioskopWithScanner28.java
+++ BioskopWithScanner28.java
@@ -7,20 +7,49 @@ public class BioskopWithScanner28 {
         String nama, next;
         String[][] penonton = new String[4][2];
         while (true) {
-        System.out.print("Masukkan nama : ");
-        nama = sc.nextLine();
-        System.out.println("Masukkan baris : ");
-        baris = sc.nextInt();
-        System.out.print("Masukkan kolom : ");
-        kolom = sc.nextInt();
-        sc.nextLine();
-        penonton[baris-1][kolom-1] = nama;
-        System.out.print("Input penonton lainnya? (y/n) : ");
-        next = sc.nextLine();
-        if (next.equalsIgnoreCase("n")){
-        break;
+            
+            System.out.println("Menu : ");
+            System.out.println("Input data penonton ");
+            System.out.println("Tampilkan daftar penonton");
+            System.out.println("Exit");
+            System.out.print("Pilih menu (1/2/3) : ");
+            
+        int menu = sc.nextInt(); switch (menu) {
+      case 1:
+            System.out.print("Masukkan nama : ");
+            nama = sc.next();
+            System.out.print("Masukkan baris : ");
+            baris = sc.nextInt();
+            System.out.print("Masukkan kolom : ");
+            kolom = sc.nextInt();
+      sc.nextLine();
+    
+      if (baris >= 1 && baris <= 4 && kolom <= 1 && kolom<=2) {
+          penonton[baris - 1][kolom - 1] = nama;
+        System.out.println("Data penonton telah dimasukkan.");
+     }else {
+        System.out.println("Baris atau kolom tidak valid.");
         }
-     }
- }
-        
+     break;
+     case 2 :
+        System.out.println("Daftar Penonton : ");
+
+        for (int i = 0; i < 4; i++){
+            for(int j = 0; j < 2; j++){
+                if (penonton[i][j] != null){
+                    System.out.println("Baris " + (i+1) + ", Kolom " + (j+1) + ": " + penonton[i][j]);
+                      }
+                   }
+            }
+            
+        break; 
+        case 3 :
+        System.out.println("Terima kasih, program telah selesai.");
+        System.exit(0);
+        default :
+        System.out.println("Menu tidak valid. Silahkan pilih 1, 2, atau 3.");
+        break;
+         }
+       }
+    }
 }
