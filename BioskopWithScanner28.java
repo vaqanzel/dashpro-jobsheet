dc00ea93ff1e2c49d673f7b23d49ce553910d29f
vaqanzel <valengrehasta@gmail.com>
2023-11-02 21:20:44 +0700

    modifikasi 3

--- BioskopWithScanner28.java
+++ BioskopWithScanner28.java
@@ -20,29 +20,30 @@ public class BioskopWithScanner28 {
         case 1:
             System.out.print("Masukkan nama : ");
             nama = sc.next();
+           do {
             System.out.print("Masukkan baris : ");
             baris = sc.nextInt();
             System.out.print("Masukkan kolom : ");
             kolom = sc.nextInt();
             sc.nextLine();
-    
-       if (baris >= 1 && baris <= 4 && kolom <= 1 && kolom<=2) {
-           penonton[baris - 1][kolom - 1] = nama;
-           System.out.println("Data penonton telah dimasukkan.");
-     }else {
-           System.out.println("Baris atau kolom tidak ditemukan.");
-        }
-     break;
-     case 2 :
-            System.out.println("Daftar Penonton : ");
+            if (isValidSeat(baris, kolom, penonton)){
+                penonton[baris - 1][kolom - 1] = nama;
+                System.out.println("Data penonton telah dimasukkan.");
+                break;
+            }else {
+                System.out.println("Kursi telah terisi. Silahkan pilih kursi lain.");
+            }
+        }while (true);
+        break;
+        case 2 :
+        System.out.println("Daftar Penonton : ");
         for (int i = 0; i < 4; i++){
             for(int j = 0; j < 2; j++){
                 if (penonton[i][j] != null){
                     System.out.println("Baris " + (i+1) + ", Kolom " + (j+1) + ": " + penonton[i][j]);
-                      }
-                   }
+                }
             }
-            
+        }
         break; 
         case 3 :
         System.out.println("Terima kasih, program telah selesai.");
@@ -50,16 +51,15 @@ public class BioskopWithScanner28 {
         default :
         System.out.println("Menu tidak valid. Silahkan pilih 1, 2, atau 3.");
         break;
-         }
-       }
     }
-    static boolean isValidSeat (int baris, int kolom, String[][]penonton){
-        if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <=2){
-            if (penonton[baris - 1][kolom-1] == null){
-                return true;
-            }
+          }
+   }
+   static boolean isValidSeat(int baris, int kolom, String[][] penonton){
+    if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <=2){
+        if (penonton[baris - 1][kolom - 1] == null){
+            return true;
         }
-        return false;
     }
+    return false;
+      }
 }
-
