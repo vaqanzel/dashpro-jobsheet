5474f5c06a2c632b8f7afe60d218fe732829ac79
vaqanzel <valengrehasta@gmail.com>
2023-11-02 22:10:21 +0700

    modif 1

--- Numbers28.java
+++ Numbers28.java
@@ -9,5 +9,8 @@ public class Numbers28 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
             }
+        for (int i = 0; i < myNumbers.length; i++){
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }      
     }
 }
